fa174fcb822d2755b1ca5d1c01c02643ab79574c
Anton Aderum <antonaderum@gmail.com>
2014-08-07 23:26:54 +0200

    hook model and new map. I love azarak

--- game/dota_addons/pudgewars/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/pudgewars/scripts/vscripts/addon_game_mode.lua
@@ -13,6 +13,7 @@ function Precache( context )
     print("hello")
     --Precache things we know we'll use.  Possible file types include (but not limited to):
     PrecacheUnitByNameSync("npc_dota_hero_pudge", context)
+    PrecacheModel("models/heroes/pudge/weapon.vmdl", context ) -- Manually precache a single model
 end
 
 -- Create the game mode when we activate
